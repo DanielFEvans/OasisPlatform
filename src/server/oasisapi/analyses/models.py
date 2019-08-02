7773ca1507e1bf17d2aa50ea181eeb360e18522c
Daniel Evans <Daniel.Evans@jbarisk.com>
2019-08-02 11:10:43 +0100

    Second variable name fix

--- src/server/oasisapi/analyses/models.py
+++ src/server/oasisapi/analyses/models.py
@@ -242,7 +242,7 @@ class Analysis(TimeStampedModel):
 
         """
         complex_data_files = [{STORED_FILENAME: cmdf.file.file.name,
-                               ORIGINAL_FILENAME: cmdf.file_name}
+                               ORIGINAL_FILENAME: cmdf.filename}
                               for cmdf in self.complex_model_data_files.all()]
         return complex_data_files
 
